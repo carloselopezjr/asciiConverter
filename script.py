10ee2bd96b17d798f422c808f16ab9e93fbd61a1
MSI <lopez212318@gmail.com>
2025-07-26 04:25:27 -0400

    Prints default + enhanced version, columnn amount input

--- script.py
+++ script.py
@@ -2,7 +2,23 @@ from ascii_magic import AsciiArt
 from PIL import ImageEnhance
 
 imageAddress = input("Enter an image address: ")
+columnAmount = int(input("\n Column Amount:"))
 
-image = AsciiArt.from_url(imageAddress)
+asciiImg = AsciiArt.from_url(imageAddress)
 
-image.to_terminal()
+asciiImg.to_terminal(columns=columnAmount)
+
+def colorEnhancements(asciiImg, columnAmount):
+
+    print("\n\nColor Enhancement Version\n\n")
+
+    asciiImg.image = ImageEnhance.Contrast(asciiImg.image).enhance(2)
+    asciiImg.image = ImageEnhance.Brightness(asciiImg.image).enhance(2.5)
+    asciiImg.image = ImageEnhance.Color(asciiImg.image).enhance(0.1)
+
+    asciiImg.to_terminal(columns=columnAmount)
+    print("\n\n")
+
+    return 
+
+colorEnhancements(asciiImg, columnAmount)
